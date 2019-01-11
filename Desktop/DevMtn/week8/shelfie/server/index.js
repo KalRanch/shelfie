60f210fec8217aad424c313a7b781de5243a14a8
KalRanch <bkbrinton@gmail.com>
2019-01-10 17:15:18 -0700

    database connection issue

--- Desktop/DevMtn/week8/shelfie/server/index.js
+++ Desktop/DevMtn/week8/shelfie/server/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
+const massive = require('massive');
+require('dotenv').config()
 
+const app = express();
+massive( process.env.CONNECTION_STRING ).then( dbInstance => {
+    app.set('db', dbInstance);
+  
+     dbInstance.new_products()
+       .then( products => console.log( products ) )
+       .catch( err => console.log( err ) );
+  });
 app.use( bodyParser.json() );
 
 const port = 4000
